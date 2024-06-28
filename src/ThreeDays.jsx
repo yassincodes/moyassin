ebc8db9c62b03c0c638176b70e362a4c9a3e5b3e
yassin <matrixexitportal@gmail.com>
2024-06-28 12:06:36 +0100

    first commit

---
+++ src/ThreeDays.jsx
@@ -0,0 +1,11 @@
+function ThreeDays() {
+    return (
+        <div>
+            GoodFriday = fix your github and blog and organise
+            saturday = journalAI finish the prototype
+            sunday = the last week
+        </div>
+    )
+}
+
+export default ThreeDays
