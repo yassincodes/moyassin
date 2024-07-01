46294fcff55ffcf556f0f5be94c0b2aa5d2531ef
yassin <matrixexitportal@gmail.com>
2024-07-01 15:16:38 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       <p>Here's a bit about my journey:</p>
       <ul>
         <li>Started writing at 12, caught the attention of <a href="https://ar.wikipedia.org/wiki/%D8%A8%D9%88%D8%B1%D8%A7%D9%88%D9%8A_%D8%B9%D8%AC%D9%8A%D9%86%D8%A9" target="_blank" rel="noopener noreferrer">بوراوي عجينة</a>, but life took different turns.</li>
-        <li>At 16, created a popular English teaching page in the Arab world.</li>
+        <li>At 16, created the most popular and interactive English teaching platform in the Arab world.</li>
         <li>Made $40 on my first day freelancing at 20, a small win but a big motivation.</li>
         <li>Once considered dropping out, but listened to my father's advice, leading to a period of introspection.</li>
         <li>Launched a startup at 21 that gained traction nationwide, though I lost the code later on.</li>
