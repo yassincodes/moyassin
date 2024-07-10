5a8658c9868efbc8cde28c360f6439fee86c0a69
yassin <matrixexitportal@gmail.com>
2024-07-10 13:45:08 +0100

    .

--- src/pages/Destiny.jsx
+++ src/pages/Destiny.jsx
@@ -9,8 +9,6 @@ function Destiny() {
             <p>Now, the scene is getting bigger and bigger. New characters will enter the stage, new adventures await, and new chess positions will be set.</p>
             <p>My greatest strength so far has been my ability to see ten versions of the future.</p>
             <p>This is why I'm building AnisAI: to give everyone this magnificent feeling of knowing what will happen, based on pure analysis of events.</p>
-            <p>But in all those versions of the future, I see only one way to remain the one who pulls the strings:</p>
-            <p>by sticking to my principles and finishing that other small product on Sunday, the 7th.</p>
         </div>
     );
 }
