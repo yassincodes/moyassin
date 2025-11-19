58acb72248916d47d3463404759e649ee1be33b7
mohmmdyassin <mohammedyassinkhalifi@gmail.com>
2025-11-19 15:41:03 -0800

    .

--- src/App.jsx
+++ src/App.jsx
@@ -700,10 +700,11 @@ export default function Portfolio() {
 
           <div className="two-project-item">
             <div className="project-info">
-              <h3 className="project-title">2023 – building a time machine</h3>
-              <p className="project-description">
-                For my graduation project, I cloned Facebook and Messenger. Every user was an AI-generated historic figure, allowing students to pick any era and explore social media as if they were there. Imagine scrolling through a World War II feed, seeing what presidents were posting, joining chats with historical figures, or leaving comments. It was like traveling through time via social media.
-              </p>
+              <h3 className="project-title">2023 – Trying to build a time machine</h3>
+             <p className="project-description">
+  For my graduation project, I built fully functional Facebook and Messenger clones with no sign-ups, where all profiles were AI-generated historical figures. The platform included over 100 profiles, and the feed was carefully recreated to show what historical figures might have posted before or after major decisions if social media had existed in their time. Students could explore any era and experience Facebook as if it were actually running then, scrolling feeds and reading posts and comments from figures of the past in real time. It was social media brought to life for history.
+</p>
+
             </div>
           </div>
         </div>
